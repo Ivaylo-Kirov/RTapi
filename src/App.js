ae138ce50b83a5f5c6b47a5e954486884831e963
Ivaylo-Kirov <ivo.kirov@gmail.com>
2019-08-24 14:07:26 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {todos: []};
+  }
+
+  componentDidMount() {
+    // fetch
+    axios.get('https://jsonplaceholder.typicode.com/todos/')
+      .then((response) => {
+        // bring it into state
+        this.setState({todos: response.data})
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  render() {
+    //console.log('in render' + this.state.todos);
+    const items = this.state.todos.length > 1 && this.state.todos.map((item) => {
+      return <li key={item.id}>{item.title}</li>
+    });
+    return(
+      // iterate
+      <div>
+        <ul>
+          {items}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
